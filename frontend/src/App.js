a846c36b64e76eb4cc1dccafcb0b53475a022976
malatesh1 <malateshpatil68@gmail.com>
2025-06-29 06:24:08 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Hello from React!</h1>
+      <p>This is a basic React app integrated with CI/CD.</p>
+    </div>
+  );
+}
+
+export default App;
